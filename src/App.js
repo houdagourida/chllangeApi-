e209f9494e039dbd2e39d2b93f4bb7c891c98212
houdagourida <@gmail.com>
2021-09-07 20:52:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import WeatherApi from './component/WeatherApi';
+
+function App() {
+  return (
+    <div className="App">
+    
+     <WeatherApi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/WeatherSearch.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WeatherApi from "./WeatherApi";
+
+
+const WeatherSearch = ({ handleSearch }) => {
+  return (
+    <div>
+      <form
+        action="/action_page.php"
+        style={{ maxWidth: "500px", margin: "auto" }}
+      >
+        <h2>
+          {" "}
+          <i className="fa fa-cloud"></i>
+          <div className="title">Weather Api</div>
+        </h2>
+        <div className="input-container">
+          <i className="fas fa-map-marker-alt"></i>
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Search"
+            name="usrnm"
+            onChange={(e) => handleSearch(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default WeatherSearch;
